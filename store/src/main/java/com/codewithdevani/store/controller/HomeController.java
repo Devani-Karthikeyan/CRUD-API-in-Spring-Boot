5c874571c9d8fedb6187001d4574e31eb12a647e
Devani-Karthikeyan <devaniselva31@gmail.com>
2026-03-03 19:09:26 +0530

    feat(auth): implement in-memory and database authentication with role-based access and password encoding

---
+++ store/src/main/java/com/codewithdevani/store/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.codewithdevani.store.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class HomeController {
+    @GetMapping
+    public String getHomePage(){
+        return "Welcome  to Home Page";
+    }
+    @GetMapping("/dashboard")
+    public String getDashboard(){
+        return "Login Successful";
+    }
+}
